3926f7b36a1c3134099b2e29f4d642f8d82cb192
Josh Furlani <Joshdog406@gmail.com>
2022-02-02 16:18:02 -0500

    Added mongodb

--- node.Dockerfile
+++ node.Dockerfile
@@ -10,4 +10,4 @@ RUN npm install
 
 EXPOSE 3000
 
-CMD ["npm", "run", "dev"]
+CMD ["npm", "run", "startDev"]
--- node/server.js
+++ node/server.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+const mongoose = require("mongoose")
+const nodes = mongoose.model("nodes", {
+  node: String,
+  u: String,
+  type: String,
+  rack: String,
+})
+
 // HTML Dependencies
 app.use(express.static("public"))
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"))
@@ -38,19 +46,38 @@ app.get("/data", (req, res) => {
     ],
   })
 })
-app.get("/rack/:rack", (req, res) => {
+
+app.get("/rack/:rack", async (req, res) => {
   const rack = req.params.rack
-  res.json({
-    nodes: [
-      { node: "cpn-" + rack + "-33", u: "33", type: "R750" },
-      { node: "cpn-" + rack + "-31", u: "31", type: "R750" },
-      { node: "cpn-" + rack + "-29", u: "29", type: "R750" },
-      { node: "cpn-" + rack + "-26", u: "26", type: "R650" },
-      { node: "cpn-" + rack + "-25", u: "25", type: "R650" },
-      { node: "cpn-" + rack + "-24", u: "24", type: "R650" },
-      { node: "cpn-" + rack + "-23", u: "23", type: "R650" },
-    ],
-  })
+  try {
+    data = await nodes.find({ rack: rack })
+    res.json({
+      nodes: data,
+    })
+  } catch (err) {
+    console.log(err)
+  }
 })
+
+dbConfig = {
+  url: "mongodb://mongodb_container:27017/dcim",
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+}
+
+mongoose.Promise = global.Promise
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+    user: dbConfig.user,
+    pass: dbConfig.pwd,
+  })
+  .then(() => {
+    console.log("successfully connected to the database")
+  })
+  .catch((err) => {
+    console.log("error connecting to the database")
+    process.exit()
+  })
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
