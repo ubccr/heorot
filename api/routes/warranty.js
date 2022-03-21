d89f40f026c7f5b9bf51666f6c97d1d6d5017d9b
Josh Furlani <Joshdog406@gmail.com>
2022-03-21 10:59:06 -0400

    added get warranty route

--- api/routes/warranty.js
+++ api/routes/warranty.js
@@ -107,51 +107,21 @@ app.get("/add/:tags", async (req, res) => {
   }
 })
 
-app.get("/refresh/:serviceTag", async (req, res) => {
-  let token = process.env.JS_DELL_WARRANTY_API_TOKEN
-  let serviceTag = req.params.serviceTag
-  let warrantyRes = await warrantyAPI(serviceTag, token)
+app.get("/get/:node", async (req, res) => {
+  const node = req.params.node
 
-  if (warrantyRes.Fault && warrantyRes.Fault.faultcode === "401") {
-    let authRes = await authAPI()
-    if (authRes.access_token) {
-      process.env.JS_DELL_WARRANTY_API_TOKEN = authRes.access_token
-      token = authRes.access_token
+  Warranty.findOne({ nodeName: node }, function (err, node) {
+    if (err) {
+      res.json({
+        status: "failed",
+        message: "An error occured while accessing the DB",
+        color: "error",
+        err,
+      })
+    } else {
+      res.json({ status: "success", result: node })
     }
-    warrantyRes = await warrantyAPI(serviceTag, token)
-  }
-  res.json({ status: "success", result: warrantyRes })
+  })
 })
-
-async function authAPI() {
-  const url = `https://apigtwb2c.us.dell.com/auth/oauth/v2/token`
-  const clientId = process.env.JS_DELL_WARRANTY_API_ID
-  const clientSecret = process.env.JS_DELL_WARRANTY_API_SECRET
-
-  let params = new URLSearchParams()
-
-  params.append("client_id", clientId)
-  params.append("client_secret", clientSecret)
-  params.append("grant_type", "client_credentials")
-
-  let payload = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: params,
-  }
-  return await (await fetch(url, payload)).json()
-}
-async function warrantyAPI(serviceTag, token) {
-  const url = `https://apigtwb2c.us.dell.com/PROD/sbil/eapi/v5/asset-entitlements?servicetags=${serviceTag}`
-  let payload = {
-    headers: {
-      Authorization: "Bearer " + token,
-      "content-type": "application/json",
-    },
-  }
-  return await (await fetch(url, payload)).json()
-}
 
 module.exports = app
