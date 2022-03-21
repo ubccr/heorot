b65bf0a0da69f0a7817111c44e7dad92ec197656
Josh Furlani <Joshdog406@gmail.com>
2022-03-21 10:46:43 -0400

    fixed shipdate

--- api/models/Warranty.js
+++ api/models/Warranty.js
@@ -5,7 +5,7 @@ const warrantySchema = new Schema(
     nodeName: { type: String, required: true, unique: true },
     bmcFqdn: { type: String, required: true },
     serviceTag: { type: String, required: true },
-    shipData: { type: String, required: true },
+    shipDate: { type: String, required: true },
     productLineDescription: { type: String, required: true },
     entitlements: { type: Array },
   },
