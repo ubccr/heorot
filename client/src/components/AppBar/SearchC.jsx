4732c9520aaf76aa4f802d9760b28312d9ecdd8f
Josh Furlani <Joshdog406@gmail.com>
2022-04-04 15:06:35 -0400

    overhauled search functionality

--- client/src/components/AppBar/SearchC.jsx
+++ client/src/components/AppBar/SearchC.jsx
@@ -1,61 +1,118 @@
-import React from "react"
-import { styled, alpha } from "@mui/material/styles"
-import InputBase from "@mui/material/InputBase"
-import SearchIcon from "@mui/icons-material/Search"
+import { TextField, Autocomplete, CircularProgress, Box } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../../contexts/UserContext"
+import { apiPort } from "../../config"
+import { useNavigate } from "react-router-dom"
 
-const SearchC = () => {
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }))
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }))
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "12ch",
-        "&:focus": {
-          width: "20ch",
-        },
-      },
-    },
-  }))
-  return (
-    <Search sx={{ marginRight: "15px" }}>
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-        placeholder="Search…"
-        inputProps={{ "aria-label": "search" }}
-      />
-    </Search>
-  )
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
 }
 
+const SearchC = () => {
+  const [open, setOpen] = useState(false)
+  const [options, setOptions] = useState([])
+  const loading = open && options.length === 0
+  const [user, setUser] = useContext(UserContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    let active = true
+
+    if (!loading) {
+      return undefined
+    }
+
+    ;(async () => {
+      let payload = {
+        headers: {
+          "x-access-token": user.accessToken,
+        },
+      }
+      await sleep(1e3)
+      let query = await (
+        await fetch(
+          `https://${window.location.hostname}:${apiPort}/grendel/host/list`,
+          payload
+        )
+      ).json()
+      if (query.grendelResponse === "success" && active) {
+        let nodes = []
+        query.response.forEach((element) => {
+          nodes.push({ name: element.name })
+        })
+        setOptions([...nodes])
+      }
+    })()
+
+    return () => {
+      active = false
+    }
+  }, [loading])
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([])
+    }
+  }, [open])
+
+  return (
+    <Box
+      sx={{
+        width: 212,
+        height: 52,
+        marginRight: "20px",
+        bgcolor: "background.main",
+        borderRadius: 2,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Autocomplete
+        sx={{
+          width: 200,
+        }}
+        size="small"
+        open={open}
+        onOpen={() => {
+          setOpen(true)
+        }}
+        onClose={() => {
+          setOpen(false)
+        }}
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        getOptionLabel={(option) => option.name}
+        options={options}
+        loading={loading}
+        clearOnBlur={true}
+        onChange={(event, value) => {
+          if (value !== null) navigate(`/Node/${value.name}`)
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading ? (
+                    <CircularProgress
+                      color="inherit"
+                      size={20}
+                      sx={{ marginRight: 4 }}
+                    />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
+        )}
+      />
+    </Box>
+  )
+}
 export default SearchC
