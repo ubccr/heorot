8943d9713dd896e10495279c4b6cda80f76aeb7f
Josh Furlani <Joshdog406@gmail.com>
2022-04-05 16:03:54 -0400

    fixed null tags bug

--- client/src/routes/Node/Index.jsx
+++ client/src/routes/Node/Index.jsx
@@ -43,6 +43,7 @@ const Index = () => {
         if (response.status === "success") {
           let data = response.result[0]
           data.provision = response.result[0].provision.toString()
+          if (data.tags === null) data.tags = []
 
           data.interfaces.forEach((val, index) => {
             if (val.fqdn.substring(0, 3) === "bmc") setBMC(val.fqdn)
