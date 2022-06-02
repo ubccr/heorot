af2a2ef54f2c2bdc644c31f66d6e0320ae09008b
Josh Furlani <Joshdog406@gmail.com>
2022-06-02 15:37:29 -0400

    added host tag and provision editing

--- client/src/routes/Grendel/EditNodes.jsx
+++ client/src/routes/Grendel/EditNodes.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import {
   Typography,
   Grid,
@@ -8,23 +8,89 @@ import {
   Select,
   MenuItem,
   Button,
+  FormHelperText,
 } from "@mui/material"
+import { useSnackbar } from "notistack"
+
+import { UserContext } from "../../contexts/UserContext"
+import { apiPort } from "../../config"
 
 const EditNodes = () => {
-  const [action, setAction] = useState("")
-  const handleChange = (e) => {
-    setAction(e.target.value)
+  const [formValues, setFormValues] = useState({ action: "", actionValue: "" })
+  const [formError, setFormError] = useState(false)
+
+  const [user, setUser] = useContext(UserContext)
+  const { enqueueSnackbar } = useSnackbar()
+
+  const setField = (field, value) => {
+    setFormValues({
+      ...formValues,
+      [field]: value,
+    })
+    if (!!formError[field])
+      setFormError({
+        ...formError,
+        [field]: null,
+      })
   }
-  const [actionValue, setActionValue] = useState("")
-  const handleValueChange = (e) => {
-    setActionValue(e.target.value)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const newErrors = validateForm()
+    if (Object.keys(newErrors).length > 0) setFormError(newErrors)
+    else {
+      let payload = {
+        headers: {
+          "x-access-token": user.accessToken,
+        },
+      }
+      let url = `https://${window.location.hostname}:${apiPort}/grendel/`
+      let nodeset = formValues.nodeset
+      if (formValues.action === "provision") {
+        if (formValues.actionValue === "true") url += `provision/${nodeset}`
+        else if (formValues.actionValue === "false")
+          url += `unprovision/${nodeset}`
+      } else if (formValues.action === "tags")
+        url += `tag/${nodeset}/${formValues.tags}`
+      fetch(url, payload)
+        .then((res) => res.json())
+        .then((result) => {
+          if (result.grendelResponse === "success")
+            enqueueSnackbar(
+              `Successfully edited ${result.response.hosts} host(s)`,
+              { variant: "success" }
+            )
+          else if (result.grendelResponse === "error")
+            enqueueSnackbar(
+              `Failed to fetch node. Response: ${result.response.message}`,
+              {
+                variant: "error",
+              }
+            )
+        })
+    }
+  }
+  function validateForm() {
+    const { nodeset, action, actionValue, tags } = formValues
+    const formErrors = {}
+
+    if (!nodeset || nodeset === "")
+      formErrors.nodeset = "Nodeset cannot be blank"
+    else if (action === "") formErrors.action = "Action cannot be blank"
+    else if (actionValue === "" && action === "provision")
+      formErrors.actionValue = "Value cannot be blank"
+    else if (!tags && action === "tags")
+      formErrors.tags = "Tags cannot be blank"
+
+    return formErrors
   }
   return (
     <>
       <Typography variant="h2" sx={{ fontSize: "16pt" }}>
         Edit Nodes:
       </Typography>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Grid
           container
           spacing={2}
@@ -39,50 +105,69 @@ const EditNodes = () => {
         >
           <Grid item xs>
             <TextField
+              error={!!formError.nodeset}
+              helperText={formError.nodeset}
+              name="nodeset"
               label="Nodeset"
               placeholder="ex: cpn-z01-[03-32]"
               sx={{ width: "100%" }}
+              onChange={(e) => setField("nodeset", e.target.value)}
             />
           </Grid>
           <Grid item xs>
             <FormControl fullWidth>
               <InputLabel id="select-label">Action</InputLabel>
               <Select
+                error={!!formError.action}
                 labelId="select-label"
-                value={action}
+                value={formValues["action"]}
                 label="Action"
-                onChange={handleChange}
+                onChange={(e) => {
+                  setField("action", e.target.value)
+                }}
               >
                 <MenuItem value={"provision"}>Set Provision</MenuItem>
                 <MenuItem value={"tags"}>Set Tags</MenuItem>
               </Select>
+              <FormHelperText error>{formError.action}</FormHelperText>
             </FormControl>
           </Grid>
           <Grid item xs>
-            {action == "provision" && (
+            {formValues.action == "provision" && (
               <FormControl sx={{ width: "100%" }}>
                 <InputLabel id="value-select-label">Value</InputLabel>
                 <Select
+                  error={!!formError.actionValue}
                   labelId="value-select-label"
-                  value={actionValue}
+                  value={formValues.actionValue}
                   label="Value"
-                  onChange={handleValueChange}
+                  onChange={(e) => {
+                    setField("actionValue", e.target.value)
+                  }}
                 >
                   <MenuItem value={"true"}>True</MenuItem>
                   <MenuItem value={"false"}>False</MenuItem>
                 </Select>
+                <FormHelperText error>{formError.actionValue}</FormHelperText>
               </FormControl>
             )}
-            {action !== "provision" && (
+            {formValues.action !== "provision" && (
               <TextField
+                error={!!formError.tags}
+                helperText={formError.tags}
                 label="Value"
                 placeholder="z01,ubhpc,gpu"
                 sx={{ width: "100%" }}
+                onChange={(e) => {
+                  setField("tags", e.target.value)
+                }}
               ></TextField>
             )}
           </Grid>
           <Grid item xs>
-            <Button variant="outlined">Submit</Button>
+            <Button variant="outlined" type="submit">
+              Submit
+            </Button>
           </Grid>
         </Grid>
       </form>
