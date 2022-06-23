e75e9e66ee77eaaeea18da1449d5ca308345a9ad
Josh Furlani <Joshdog406@gmail.com>
2022-06-23 16:13:54 -0400

    started adding user info

--- client/src/routes/Profile/Profile.jsx
+++ client/src/routes/Profile/Profile.jsx
@@ -1,6 +1,9 @@
 import { Typography, Container } from "@mui/material"
+import { UserContext } from "../../contexts/UserContext"
+import { useEffect, useState, useContext } from "react"
 
 const Profile = () => {
+  const [user, setUser] = useContext(UserContext)
   return (
     <>
       <Container style={{ textAlign: "center" }}>
