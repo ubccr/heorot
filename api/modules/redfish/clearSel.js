375c60ab43d1e10823b653f83d9e02b347571f69
jafurlan <joshdog406@gmail.com>
2022-07-27 17:12:19 -0400

    added clear sel route

--- api/modules/redfish/clearSel.js
+++ api/modules/redfish/clearSel.js
@@ -1,25 +1,60 @@
 const { api_request } = require("./redfish")
 
+const body = JSON.stringify({ Action: "ClearLog" })
+
 async function dell_clearSel(uri, token) {
   const url =
     uri +
     "/redfish/v1/Managers/iDRAC.Embedded.1/LogServices/Sel/Actions/LogService.ClearLog"
-
-  await api_request(url, token, "POST")
-  return {
-    status: "success",
-    message: "System Event Log successfully cleared",
+  try {
+    let res = await api_request(url, token, "POST", false, body)
+    return {
+      status: res.status,
+      message: "System Event Log successfully cleared",
+    }
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Error clearing SEL",
+      error,
+    }
   }
 }
 
 async function sm_clearSel(uri, token) {
   const url =
-    uri + "/redfish/v1/Systems/1/LogServices/IML/Actions/LogService.ClearLog"
+    uri + "/redfish/v1/Systems/1/LogServices/Log1/Actions/LogService.ClearLog"
+  try {
+    let res = await api_request(url, token, "POST", false, body)
+    return {
+      status: res.status,
+      message: "System Event Log successfully cleared",
+    }
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Error clearing SEL",
+      error,
+    }
+  }
 }
 
 async function hpe_clearSel(uri, token) {
   const url =
-    uri + "/redfish/v1/Systems/1/LogServices/Log1/Actions/LogService.ClearLog"
+    uri + "/redfish/v1/Systems/1/LogServices/IML/Actions/LogService.ClearLog/"
+  try {
+    let res = await api_request(url, token, "POST", false, body)
+    return {
+      status: res.status,
+      message: "System Event Log successfully cleared",
+    }
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Error clearing SEL",
+      error,
+    }
+  }
 }
 
 module.exports = {
