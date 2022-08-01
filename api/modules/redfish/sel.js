0e474971aa0016a1b6cec67eacdab33ec3d329f4
jafurlan <joshdog406@gmail.com>
2022-08-01 15:43:29 -0400

    fixed sel query for old dell nodes

--- api/modules/redfish/sel.js
+++ api/modules/redfish/sel.js
@@ -1,25 +1,47 @@
 const { api_request } = require("./redfish")
 
-async function dell_sel(uri, token) {
-  let top = "10"
-  let url = uri + `/redfish/v1/Managers/iDRAC.Embedded.1/Logs/Sel/?$top=${top}`
+async function dell_sel(uri, token, version) {
+  let versionArr = version.split(".")
+  if (versionArr[0] === 1 && versionArr[1] < 4) {
+    let url = uri + `/redfish/v1/Managers/iDRAC.Embedded.1/Logs/Sel`
+    let res = await api_request(url, token)
 
-  let res = await api_request(url, token)
-
-  if (res.status === "success") {
-    return {
-      status: res.status,
-      count: res.data["Members@odata.count"],
-      logs: res.data.Members.map((val) => {
-        return {
-          created: val.Created,
-          message: val.Message,
-          severity: val.Severity,
-        }
-      }),
-    }
+    if (res.status === "success") {
+      return {
+        status: res.status,
+        count: res.data["Members@odata.count"],
+        logs: res.data.Members.map((val) => {
+          return {
+            created: val.Created,
+            message: val.Message,
+            severity: val.Severity,
+          }
+        }),
+      }
+    } else return res
   } else {
-    if (res.error["@Message.ExtendedInfo"][0].MessageArgs.length > 0) {
+    let top = "10"
+    let url =
+      uri + `/redfish/v1/Managers/iDRAC.Embedded.1/Logs/Sel/?$top=${top}`
+
+    let res = await api_request(url, token)
+
+    if (res.status === "success") {
+      return {
+        status: res.status,
+        count: res.data["Members@odata.count"],
+        logs: res.data.Members.map((val) => {
+          return {
+            created: val.Created,
+            message: val.Message,
+            severity: val.Severity,
+          }
+        }),
+      }
+    } else if (
+      res.error["@Message.ExtendedInfo"][0] !== undefined &&
+      res.error["@Message.ExtendedInfo"][0].MessageArgs.length > 0
+    ) {
       top = res.error["@Message.ExtendedInfo"][0].MessageArgs[2]
         .replace(")", "")
         .split("-")[1]
