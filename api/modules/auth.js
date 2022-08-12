8a31a9eacd960872701db0f9bcaf539d2b30db96
jafurlan <joshdog406@gmail.com>
2022-08-12 15:47:37 -0400

    fixed auth bypass

--- api/modules/auth.js
+++ api/modules/auth.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken")
 let config = require("../config")
+const User = require("../models/User")
 
 function auth(req, res, next) {
-  if (config.environment === "dev") {
+  if ("prod" === "dev") {
     return next()
   } else {
     let token = req.headers["x-access-token"]
@@ -11,15 +12,23 @@ function auth(req, res, next) {
         .status(403)
         .send({ status: "error", message: "No auth token provided" })
     }
-    jwt.verify(token, config.auth.API_JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, config.auth.API_JWT_SECRET, async (err, decoded) => {
       if (err) {
         return res.status(401).send({
           status: "error",
           message: "Authorization failed. Please login again",
         })
       }
-      req.userId = decoded.id
-      next()
+      let query = await User.findOne({ _id: decoded.id }).exec()
+      console.log(query)
+      if (query.privileges !== "none") {
+        req.userId = decoded.id
+        next()
+      } else {
+        return res
+          .status(403)
+          .send({ status: "error", message: "Insufficient privileges" })
+      }
     })
   }
 }
