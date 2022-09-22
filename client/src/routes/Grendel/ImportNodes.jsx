764e163e22c9cdbda91fd7b93194d021fb03ab42
jafurlan <joshdog406@gmail.com>
2022-09-21 21:07:06 -0400

    started rewriting bulk node imports

--- client/src/routes/Grendel/ImportNodes.jsx
+++ client/src/routes/Grendel/ImportNodes.jsx
@@ -1,206 +1,107 @@
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogTitle,
-  FormGroup,
-  FormHelperText,
-  Grid,
-  IconButton,
-  TextField,
-  TextareaAutosize,
-  Typography,
-} from "@mui/material"
-import { useContext, useState } from "react"
+import { Button, Checkbox, FormGroup, Grid, TextField } from "@mui/material"
+import React, { useContext, useState } from "react"
 
-import CloseIcon from "@mui/icons-material/Close"
 import { UserContext } from "../../contexts/UserContext"
 import { apiConfig } from "../../config"
+import { useQuery } from "react-query"
 import { useSnackbar } from "notistack"
 
 const ImportNodes = () => {
-  const [user, setUser] = useContext(UserContext)
+  const [user] = useContext(UserContext)
   const { enqueueSnackbar } = useSnackbar()
-  const [open, setOpen] = useState(false)
-  const [nodeJson, setNodeJson] = useState("")
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  let textFieldProps = { fullWidth: true, autoComplete: "new-password", variant: "outlined", size: "small" }
+  let buttonProps = { variant: "outlined" }
 
-    let form = event.target
-    let payload = {
-      method: "POST",
-      headers: {
-        "x-access-token": user.accessToken,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        sw: form.switch.value,
-        domain: form.domain.value,
-        subnet: form.subnet.value,
-        bmcSubnet: form.bmcSubnet.value,
-        mapping: form.mapping.value,
-      }),
-    }
-    fetch(`${apiConfig.apiUrl}/grendel/discover`, payload)
-      .then((res) => res.json())
-      .then((result) => {
-        if (result.status === "success") {
-          enqueueSnackbar(`Successfully discovered hosts`, {
-            variant: "success",
-          })
-          setOpen(true)
-          setNodeJson(JSON.stringify(result.node, null, 4))
-        } else if (result.status === "error")
-          enqueueSnackbar(`${result.error}`, {
-            variant: "error",
-          })
-      })
-  }
-  const handleTab = (e) => {
-    if (e.keyCode === 9 && !e.shiftKey) {
-      e.preventDefault()
-      let val = e.target.value,
-        start = e.target.selectionStart,
-        end = e.target.selectionEnd
+  const [sw, setSw] = useState("")
+  const [bmcSw, setBmcSw] = useState("")
+  const [field, setField] = useState("")
 
-      e.target.value = val.substring(0, start) + "\t" + val.substring(end)
-      e.target.selectionStart = e.target.selectionEnd = start + 1
-    }
-  }
-  const handleJson = () => {
-    try {
-      let a = JSON.parse(nodeJson) // just to generate an error
+  let tmp = [
+    { label: "Switch", placeholder: "swe-z01-22", onChange: (e) => setSw(e.target.value), ...textFieldProps },
+    { label: "BMC Switch", placeholder: "swe-z01-42", onChange: (e) => setBmcSw(e.target.value), ...textFieldProps },
+    { label: "Firmware", ...textFieldProps },
+    { label: "Boot Image", ...textFieldProps },
+    { label: "Provision", ...textFieldProps },
+    { label: "Tags", placeholder: "ubhpc,z01,gpu", ...textFieldProps },
+  ]
+
+  const switchQuery = useQuery(
+    ["bulkNodeImportSw", sw],
+    async ({ signal }) => {
       let payload = {
-        method: "POST",
         headers: {
           "x-access-token": user.accessToken,
-          "Content-Type": "application/json",
         },
-        body: nodeJson,
+        signal,
       }
-      fetch(`${apiConfig.apiUrl}/grendel/host`, payload)
-        .then((res) => res.json())
-        .then((result) => {
-          if (result.status === "success") {
-            enqueueSnackbar(`Successfully added ${result.result.hosts} host(s)`, {
-              variant: "success",
-            })
-          } else if (result.status === "error")
-            enqueueSnackbar(`Failed to edit node. Response: ${result.result.message}`, {
-              variant: "error",
-            })
-        })
-    } catch (e) {
-      enqueueSnackbar(`Error in JSON syntax: ${e}`, { variant: "error" })
+      const res = await (await fetch(`${apiConfig.apiUrl}/switches/v1/query/${sw}`, payload)).json()
+      if (res.status === "error") enqueueSnackbar(res.message, { variant: "error" })
+      return res
+    },
+    { enabled: false }
+  )
+  const switchBmcQuery = useQuery(
+    ["bulkNodeImportSwBmc", bmcSw],
+    async ({ signal }) => {
+      let payload = {
+        headers: {
+          "x-access-token": user.accessToken,
+        },
+        signal,
+      }
+      const res = await (await fetch(`${apiConfig.apiUrl}/switches/v1/query/${bmcSw}/true`, payload)).json()
+      if (res.status === "error") enqueueSnackbar(res.message, { variant: "error" })
+      return res
+    },
+    { enabled: false }
+  )
+  const handleQuery = async () => {
+    let mapping = field.split("\n").map((node) => node.split(":").map((port) => port.split(",")))
+
+    let swQueryRes = null
+    let swBmcQueryRes = null
+    if (sw !== "") swQueryRes = await switchQuery.refetch()
+    if (bmcSw !== "") swBmcQueryRes = await switchBmcQuery.refetch()
+
+    if (swQueryRes.status === "success" && swQueryRes.data.status === "success") {
+      mapping.map((map) => {
+        let MAT = swQueryRes.data.result[2].output
+        let port = MAT.map((val) => {
+          if (val.port === map[1][0]) return val
+        }).filter(Boolean)
+        console.log(port)
+      })
     }
+  }
+  function delay(time) {
+    return new Promise((resolve) => setTimeout(resolve, time))
   }
   return (
     <>
-      <Typography variant="h2" sx={{ fontSize: "16pt", marginBottom: "10px" }}>
-        Add Nodes:
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Grid container spacing={2}>
-            <Grid item xs>
-              <TextField
-                fullWidth
-                label="Switch"
-                name="switch"
-                placeholder="swe-z01-22"
-                required
-                autoComplete="new-password"
-              />
+      <FormGroup>
+        <Grid container spacing={2}>
+          {tmp.map((val, index) => (
+            <Grid item xs={4} key={index}>
+              <TextField {...val} />
             </Grid>
-            <Grid item xs>
-              <TextField
-                fullWidth
-                label="Domain"
-                defaultValue={apiConfig.grendelDomain}
-                name="domain"
-                required
-                autoComplete="new-password"
-              />
-            </Grid>
-            <Grid item xs>
-              <TextField
-                fullWidth
-                label="Subnet"
-                name="subnet"
-                defaultValue={apiConfig.grendelSubnet}
-                required
-                autoComplete="new-password"
-              />
-            </Grid>
-            <Grid item xs>
-              <TextField
-                fullWidth
-                label="BMC Subnet"
-                defaultValue={apiConfig.grendelBmcSubnet}
-                name="bmcSubnet"
-                required
-                autoComplete="new-password"
-              />
-            </Grid>
+          ))}
+          <Grid item xs={12}></Grid>
+          <Grid item xs={4}>
+            <TextField
+              placeholder="(Node:Port1,Port2) ex: cpn-z01-01:20,28"
+              onKeyUp={(e) => setField(e.target.value)}
+              fullWidth
+              multiline
+            />
           </Grid>
-          <Box sx={{ display: "inline-flex" }}>
-            <Typography
-              variant="h2"
-              sx={{
-                fontSize: "12pt",
-                marginTop: "20px",
-                marginLeft: "10px",
-                marginRight: "10px",
-              }}
-            >
-              Host Mapping:
-            </Typography>
-            <Button variant="outlined" size="small" type="submit" sx={{ marginTop: "15px" }}>
-              Submit
+          <Grid item xs={2}>
+            <Button {...buttonProps} onClick={handleQuery}>
+              Query Switches
             </Button>
-          </Box>
-          <TextField
-            multiline
-            name="mapping"
-            placeholder="tab delimited"
-            required
-            onKeyDown={(e) => handleTab(e)}
-            sx={{ margin: "10px" }}
-          />
-        </FormGroup>
-      </form>
-      <Dialog open={open} onClose={() => setOpen(false)} maxWidth="md" fullWidth scroll="paper">
-        <DialogTitle>
-          Discovered Nodes:
-          <IconButton
-            aria-label="close"
-            onClick={() => setOpen(false)}
-            sx={{
-              position: "absolute",
-              right: 8,
-              top: 8,
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </DialogTitle>
-        <TextField
-          multiline
-          value={nodeJson}
-          onChange={(e) => {
-            setNodeJson(e.target.value)
-          }}
-          sx={{ margin: "10px" }}
-        />
-
-        <DialogActions>
-          <Button variant="outlined" onClick={handleJson}>
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
+          </Grid>
+        </Grid>
+      </FormGroup>
     </>
   )
 }
