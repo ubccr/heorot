80e8dd4c5d7a5a5dca47b804744b2af31b9b8aac
jafurlan <joshdog406@gmail.com>
2022-09-26 11:03:13 -0400

    updated config

--- api/config.example.js
+++ api/config.example.js
@@ -1,20 +1,18 @@
 let config = {}
 
-config.environment = "prod" // dev | prod - allows you to make api requests without auth
-config.origin = "https://localhost:443" // If using separate API & Client change to client addr
+config.environment = "prod" // dev | prod - dev allows you to make api requests without auth
+config.origin = "https://localhost:443" // Only change if you are using a separate Client and API
 config.port = 443 // API port
 
-// Keys - see readme in keys api/keys dir
-// Console redirection can now be used with password based auth
+// Keys - see readme -> Keys for more info
 config.keys = {
-  bmcKey: "./keys/bmc.key",
   serverKey: "./keys/server.key",
   serverCert: "./keys/server.cert",
 }
 
 // Auth
 config.auth = {
-  API_JWT_SECRET: "", // random string used as a JWT secret
+  API_JWT_SECRET: "", // Please enter a random string used as a JWT secret | You can use: openssl rand -base64 32
   WARRANTY_API_TOKEN: "", // leave blank
   WARRANTY_API_ID: "", // Dell developer API ID
   WARRANTY_API_SECRET: "", // Dell developer API Secret
@@ -22,8 +20,8 @@ config.auth = {
 
 // BMC
 config.bmc = {
-  DELL_USER: "", // set to your nodes idrac user - used for Redfish API requests
-  DELL_PASS: "", // set to your nodes idrac password
+  DELL_USER: "", // set to your nodes idrac/redfish user
+  DELL_PASS: "", // set to your nodes idrac/redfish password
 }
 
 // Database - configure for your MongoDB install
@@ -40,21 +38,19 @@ config.db = {
 // Grendel
 config.grendel = {
   socket: "/var/lib/grendel/grendel-api.socket", // path to Grendel UNIX socket **absolute path** (user nodejs is running as needs permissions to access {add to grendel group})
-  configPath: "/etc/grendel/grendel.toml", // path to grendel config file
-  mappingName: "/opt/heorot/api/mapping.txt", // path to writable directory for mapping file
 }
 
 // OpenManage Enterprise
 config.ome = {
-  url: "", // OME api url
-  user: "", // OME api username
-  pass: "", // OME api password
+  url: "",
+  user: "",
+  pass: "",
 }
 
 // Grendel pxe firmware list | changes the firmware options shown in lists
 config.firmware = ["ipxe.pxe", "ipxe-i386.efi", "ipxe-x86_64.efi", "snponly-x86_64.efi", "undionly.kpxe"]
 
-// Switch login information for switch queries | Should be a user with privileges to run ex: show mac address-table, show interfaces status, show ivnentory
+// Login information for switch queries | Should be a user with privileges to run ex: show mac address-table, show interfaces status, show inventory
 // Switches will need proper tags | see readme.MD -> Grendel Tags
 config.switches = {
   user: "",
