c2cee6cd1321cbb16063f56b5b37e5f95186190e
jafurlan <joshdog406@gmail.com>
2022-09-26 14:32:49 -0400

    fixed single digit vlans

--- client/src/routes/Grendel/ImportNodes.jsx
+++ client/src/routes/Grendel/ImportNodes.jsx
@@ -195,7 +195,8 @@ const ImportNodes = () => {
               ip += "128." // our bmc subnet
               fqdn = `bmc${map[0][0].substring(3)}.${form.domain}`
             }
-            ip += val.vlan.slice(-2) + "."
+            let tmpVlan = val.vlan.slice(-2).substring(0, 1) === "0" ? val.vlan.slice(-1) : val.vlan.slice(-2)
+            ip += tmpVlan + "."
             ip += val.port
 
             return {
