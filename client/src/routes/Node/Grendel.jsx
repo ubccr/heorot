f5c505ef60fa8da5f8c92ce442634c54f7c3d510
jafurlan <joshdog406@gmail.com>
2023-01-12 21:01:13 -0500

    fixed interfaces

--- client/src/routes/Node/Grendel.jsx
+++ client/src/routes/Node/Grendel.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Checkbox,
+  Chip,
   Collapse,
   FormControl,
   IconButton,
@@ -16,44 +17,76 @@ import {
   TextField,
   Typography,
 } from "@mui/material"
-import { Controller, useForm } from "react-hook-form"
+import { Controller, useFieldArray, useForm } from "react-hook-form"
+import React, { useEffect } from "react"
 
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined"
+import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined"
+import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined"
 import CloseOutlined from "@mui/icons-material/CloseOutlined"
 import ExpandLessIcon from "@mui/icons-material/ExpandLess"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import React from "react"
+import { useAutoAnimate } from "@formkit/auto-animate/react"
 import { useState } from "react"
 
 const Grendel = ({ query }) => {
-  const [default_values, setDefault_values] = useState({
-    provision: query.data.result.provision,
-    firmware: query.data.result.firmware,
-    boot_image: query.data.result.boot_image,
-    tags: query.data.result.tags.join(", "),
-    interfaces: query.data.result.interfaces.map((val) => {
-      return {
+  const [tagRef] = useAutoAnimate(null)
+  const [tagExpand, setTagExpand] = useState(false)
+  const [newTag, setNewTag] = useState("")
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      provision: query.data.result.provision,
+      firmware: query.data.result.firmware,
+      boot_image: query.data.result.boot_image,
+      tags: query.data.result.tags.join(", "),
+    },
+  })
+
+  const {
+    fields: interface_fields,
+    append: interface_append,
+    remove: interface_remove,
+  } = useFieldArray({
+    control,
+    name: "interfaces",
+  })
+  const {
+    fields: tag_fields,
+    append: tag_append,
+    remove: tag_remove,
+  } = useFieldArray({
+    control,
+    name: "tags",
+  })
+
+  useEffect(() => {
+    interface_remove()
+    tag_remove()
+    query.data.result.interfaces.forEach((val) => {
+      interface_append({
         mac: val.mac,
         ip: val.ip,
         ifname: val.ifname,
         fqdn: val.fqdn,
         bmc: val.bmc,
         vlan: val.vlan,
-      }
-    }),
-  })
+      })
+    })
+    query.data.result.tags.forEach((val) => {
+      tag_append({
+        tag: val,
+      })
+    })
+  }, [query])
 
-  const { control, unregister, handleSubmit } = useForm({
-    defaultValues: default_values,
-  })
   const onSubmit = (data) => {
     console.log(data)
   }
-  console.log(query.data)
-  console.log(default_values)
   return (
     <TableContainer>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Table>
+        <Table size="small">
           <TableHead>
             <TableRow>
               <TableCell></TableCell>
@@ -64,20 +97,14 @@ const Grendel = ({ query }) => {
                 <Button
                   variant="outlined"
                   size="small"
-                  onClick={async () => {
-                    setDefault_values({
-                      ...default_values,
-                      interfaces: [
-                        ...default_values.interfaces,
-                        {
-                          mac: "",
-                          ip: "",
-                          ifname: "",
-                          fqdn: "",
-                          bmc: false,
-                          vlan: "",
-                        },
-                      ],
+                  onClick={() => {
+                    interface_append({
+                      mac: "",
+                      ip: "",
+                      ifname: "",
+                      fqdn: "",
+                      bmc: false,
+                      vlan: "",
                     })
                   }}
                 >
@@ -87,11 +114,6 @@ const Grendel = ({ query }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell>ID:</TableCell>
-              <TableCell align="right">{query.data.result.id}</TableCell>
-            </TableRow>
-
             <TableRow>
               <TableCell>Provision:</TableCell>
               <TableCell align="right">
@@ -106,11 +128,26 @@ const Grendel = ({ query }) => {
             <TableRow>
               <TableCell>Tags:</TableCell>
               <TableCell align="right">
-                <Controller
-                  name="tags"
-                  control={control}
-                  render={({ field }) => <TextField size="small" {...field} />}
-                />
+                <Box sx={{ display: "inline-flex", gap: "4px", alignItems: "center" }} ref={tagRef}>
+                  {tag_fields.map((item, index) => (
+                    <Chip key={item.id} label={tag_fields[index].tag} onDelete={() => tag_remove(index)} />
+                  ))}
+                  <Collapse in={tagExpand} unmountOnExit>
+                    <TextField
+                      sx={{ width: "80px" }}
+                      size="small"
+                      placeholder="z01"
+                      onChange={(e) => setNewTag(e.target.value)}
+                    />
+                    <IconButton onClick={() => tag_append({ tag: newTag })}>
+                      <AddOutlinedIcon />
+                    </IconButton>
+                  </Collapse>
+                  <IconButton size="small" onClick={() => setTagExpand(!tagExpand)}>
+                    {tagExpand === false && <ChevronLeftOutlinedIcon />}
+                    {tagExpand === true && <ChevronRightOutlinedIcon />}
+                  </IconButton>
+                </Box>
               </TableCell>
             </TableRow>
 
@@ -135,6 +172,22 @@ const Grendel = ({ query }) => {
               </TableCell>
             </TableRow>
 
+            {interface_fields.map((item, index) => (
+              <TableRow key={item.id}>
+                <TableCell>Interface {index + 1}</TableCell>
+                <TableCell align="right">
+                  <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                    <Box>
+                      <Interfaces data={item} index={index} control={control} />
+                    </Box>
+                    <IconButton size="small" onClick={() => interface_remove(index)}>
+                      <CloseOutlined />
+                    </IconButton>
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+
             <TableRow>
               <TableCell>Firmware:</TableCell>
               <TableCell align="right">
@@ -156,30 +209,10 @@ const Grendel = ({ query }) => {
               </TableCell>
             </TableRow>
 
-            {default_values.interfaces.map((val, index) => (
-              <TableRow key={index}>
-                <TableCell>Interface {index + 1}</TableCell>
-                <TableCell align="right">
-                  <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                    <Box>
-                      <Interfaces data={val} index={index} control={control} />
-                    </Box>
-                    <IconButton
-                      size="small"
-                      onClick={() => {
-                        // FIXME:
-                        unregister(`interfaces[${index}]`)
-                        let tmp = default_values
-                        tmp.interfaces.splice(index, 1)
-                        setDefault_values({ ...tmp })
-                      }}
-                    >
-                      <CloseOutlined />
-                    </IconButton>
-                  </Box>
-                </TableCell>
-              </TableRow>
-            ))}
+            <TableRow>
+              <TableCell>ID:</TableCell>
+              <TableCell align="right">{query.data.result.id}</TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </form>
@@ -210,7 +243,6 @@ const Interfaces = ({ data, index, control }) => {
                   <Controller
                     name={`interfaces.${index}.ifname`}
                     control={control}
-                    defaultValue={""}
                     render={({ field }) => <TextField size="small" {...field} fullWidth />}
                   />
                 </TableCell>
@@ -221,7 +253,6 @@ const Interfaces = ({ data, index, control }) => {
                   <Controller
                     name={`interfaces.${index}.fqdn`}
                     control={control}
-                    defaultValue={""}
                     render={({ field }) => <TextField size="small" {...field} fullWidth />}
                   />
                 </TableCell>
@@ -232,7 +263,6 @@ const Interfaces = ({ data, index, control }) => {
                   <Controller
                     name={`interfaces.${index}.ip`}
                     control={control}
-                    defaultValue={""}
                     render={({ field }) => <TextField size="small" {...field} fullWidth />}
                   />
                 </TableCell>
@@ -243,7 +273,6 @@ const Interfaces = ({ data, index, control }) => {
                   <Controller
                     name={`interfaces.${index}.mac`}
                     control={control}
-                    defaultValue={""}
                     render={({ field }) => <TextField size="small" {...field} fullWidth />}
                   />
                 </TableCell>
@@ -254,7 +283,6 @@ const Interfaces = ({ data, index, control }) => {
                   <Controller
                     name={`interfaces.${index}.vlan`}
                     control={control}
-                    defaultValue={""}
                     render={({ field }) => <TextField size="small" {...field} fullWidth />}
                   />
                 </TableCell>
@@ -265,7 +293,6 @@ const Interfaces = ({ data, index, control }) => {
                   <Controller
                     name={`interfaces.${index}.mtu`}
                     control={control}
-                    defaultValue={""}
                     render={({ field }) => <TextField size="small" {...field} fullWidth />}
                   />
                 </TableCell>
