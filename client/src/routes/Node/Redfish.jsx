16704dd0faf98bd0e335497cf280c75419f0deea
jafurlan <joshdog406@gmail.com>
2023-01-17 16:59:26 -0500

    continued work on new redfish page

--- client/src/routes/Node/Redfish.jsx
+++ client/src/routes/Node/Redfish.jsx
@@ -1,6 +1,8 @@
 import {
   Box,
+  Button,
   Card,
+  CardActions,
   CardContent,
   Collapse,
   Divider,
@@ -13,7 +15,8 @@ import {
   TableRow,
   Typography,
 } from "@mui/material"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { green, grey, orange } from "@mui/material/colors"
 
 import ExpandLessIcon from "@mui/icons-material/ExpandLess"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
@@ -21,27 +24,51 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 
 const Redfish = ({ query }) => {
   let data = query.data.redfish
+
+  const [diskChassisArr, setDiskChassisArr] = useState([])
+  useEffect(() => {
+    // This should probably be server side
+    if (
+      !!data &&
+      data.status !== "error" &&
+      data.storage.slotCount !== null &&
+      data.storage.drives[0].slot !== undefined
+    ) {
+      let slotCount_num = Number(data.storage.slot_count)
+      let tmp_arr = new Array(slotCount_num)
+      for (let x = 0; x < slotCount_num; x++) {
+        let drive = data.storage.drives.find((val) => Number(val.slot) === x)
+        if (drive) tmp_arr[x] = drive
+        else tmp_arr[x] = { name: "", status: "empty" }
+      }
+      setDiskChassisArr(tmp_arr)
+    } else if (!!data) {
+      setDiskChassisArr(data.storage.drives)
+    }
+  }, [query])
+
   if (data === undefined)
     return (
       <Typography variant="h1" fontSize={20}>
         No Redfish data found!
       </Typography>
     )
-  if (data.error !== undefined)
+  if (data.status === "error")
     return (
       <Typography variant="h1" fontSize={20}>
-        {" "}
-        Error querying node: {data.error?.code}
+        Error querying node: {data.error === undefined ? data.message : data.error?.code}
       </Typography>
     )
+
   return (
     <>
       <Box sx={{ width: "100%" }}>
         <Grid2 container spacing={1}>
-          <Grid2 xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined">
+          <Grid2 xs={12} sm={6} lg={3}>
+            <Card variant="outlined" sx={{ height: "190px" }}>
+              {/* <CardMedia sx={{ height: "160px" }} image={dell_r740} /> */}
               <CardContent>
-                <Typography variant="h1" fontSize={18}>
+                <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
                   Model: {data.model}
                 </Typography>
                 <Typography variant="h2" fontSize={16}>
@@ -50,66 +77,31 @@ const Redfish = ({ query }) => {
                 <Typography variant="h2" fontSize={16}>
                   Service Tag: {data.service_tag}
                 </Typography>
-              </CardContent>
-            </Card>
-          </Grid2>
-
-          <Grid2 xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined">
-              <CardContent>
-                <Typography variant="h1" fontSize={18}>
+                {/* <Divider /> */}
+                <Typography variant="h1" fontSize={14}>
                   BIOS Version: {data.bios_version}
                 </Typography>
-                <Typography variant="h2" fontSize={16}>
+                <Typography variant="h2" fontSize={14} sx={{ marginBottom: "5px" }}>
                   BMC Version: {data.bmc.version}
                 </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  Power State: {data.power_state}
+                <Typography variant="h1" fontSize={16}>
+                  Memory: {data.memory.size} at {data.memory.speed}
+                </Typography>
+                <Typography variant="h1" fontSize={14}>
+                  Boot Order: {data.boot_order}
                 </Typography>
               </CardContent>
             </Card>
           </Grid2>
 
-          <Grid2 xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined">
+          <Grid2 xs={12} sm={6} lg={3}>
+            <Card variant="outlined" sx={{ height: "190px" }}>
               <CardContent>
-                <Typography variant="h1" fontSize={18}>
-                  CPU Count: {data.processor.length}
+                <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
+                  BMC
                 </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  Model: {data.processor[0]?.model}
-                </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  Cores: {data.processor[0]?.cores}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid2>
-
-          <Grid2 xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined">
-              <CardContent>
-                <Typography variant="h1" fontSize={18}>
-                  GPU Count: {data.gpu.physical}
-                </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  Manufacturer: {data.gpu.gpus[0]?.manufacturer}
-                </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  Model: {data.gpu.gpus[0]?.model}
-                </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  SR-IOV: {data.gpu.vGPU ? "true" : "false"}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid2>
-
-          <Grid2 xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined">
-              <CardContent>
-                <Typography variant="h1" fontSize={18}>
-                  BMC address: {data.bmc.addresses.ip} ({data.bmc.addresses.type})
+                <Typography variant="h1" fontSize={16}>
+                  Address: {data.bmc.addresses.ip} ({data.bmc.addresses.type})
                 </Typography>
                 <Typography variant="h2" fontSize={16}>
                   Gateway: {data.bmc.addresses.gateway}
@@ -124,18 +116,183 @@ const Redfish = ({ query }) => {
             </Card>
           </Grid2>
 
-          <Grid2 xs={12} sm={6} md={4} lg={3}>
-            <Card variant="outlined">
+          <Grid2 xs={12} sm={6} lg={3}>
+            <Card variant="outlined" sx={{ height: "190px" }}>
               <CardContent>
-                <Typography variant="h1" fontSize={18}>
-                  Memory: {data.memory.size} {data.memory.speed}
+                <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
+                  Volumes
                 </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  Memory Status: {data.memory.status}
+                {data.storage.volumes.map((val, index) => (
+                  <React.Fragment key={index}>
+                    <Typography variant="h1" fontSize={16}>
+                      Name: {val.name}
+                    </Typography>
+                    <Typography variant="h1" fontSize={14}>
+                      Type: {val.volume_type} {val.raid_type}
+                    </Typography>
+                    <Typography variant="h1" fontSize={14} sx={{ marginBottom: "5px" }}>
+                      Capacity: {val.capacity}
+                    </Typography>
+                  </React.Fragment>
+                ))}
+              </CardContent>
+            </Card>
+          </Grid2>
+
+          <Grid2 xs={12} sm={6} lg={3}>
+            <Card variant="outlined" sx={{ height: "190px" }}>
+              <CardContent>
+                <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
+                  System Event Log
                 </Typography>
-                <Typography variant="h2" fontSize={16}>
-                  SEL Count: {data.sel.count}
+                <Typography variant="h1" fontSize={16}>
+                  Count: {data.sel.count}
                 </Typography>
+                <Typography variant="h1" fontSize={14}>
+                  Latest Log: {data.sel.logs[0]?.message}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="outlined" size="small">
+                  Show
+                </Button>
+                <Button variant="outlined" size="small">
+                  Clear
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid2>
+
+          <Grid2 xs={12}>
+            <Divider />
+          </Grid2>
+
+          {data.processor.length > 0 && (
+            <Grid2 xs={12} sm={6} lg={3}>
+              <Card variant="outlined" sx={{ height: "270px" }}>
+                <CardContent>
+                  {/* <CardMedia sx={{ height: "160px", marginBottom: "8px" }} image={intel_logo} /> */}
+                  <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
+                    CPUs
+                  </Typography>
+                  {data.processor.map((val, index) => (
+                    <React.Fragment key={index}>
+                      <Typography variant="h2" fontSize={16}>
+                        {index + 1}: {val.model.split("@")[0]}
+                      </Typography>
+                      <Typography variant="h2" fontSize={14} sx={{ textIndent: "20px" }}>
+                        Cores: {val.cores}
+                      </Typography>
+                      {val.logical_proc === "Enabled" && (
+                        <Typography variant="h2" fontSize={14} sx={{ textIndent: "20px" }}>
+                          Threads: {val.threads}
+                        </Typography>
+                      )}
+                      {/* {val.turbo === "Enabled" && ( */}
+                      <Typography variant="h2" fontSize={14} sx={{ textIndent: "20px" }}>
+                        Frequency: {val.frequency} MHz
+                      </Typography>
+                      <Typography variant="h2" fontSize={14} sx={{ textIndent: "20px", marginBottom: "10px" }}>
+                        Turbo Frequency: {val.max_frequency} MHz
+                      </Typography>
+                    </React.Fragment>
+                  ))}
+                </CardContent>
+              </Card>
+            </Grid2>
+          )}
+
+          {data.gpu.gpus.length > 0 && (
+            <Grid2 xs={12} sm={6} lg={3}>
+              <Card variant="outlined" sx={{ height: "270px" }}>
+                <CardContent>
+                  {/* <CardMedia sx={{ height: "160px", marginBottom: "8px" }} image={nvidia_logo} /> */}
+                  <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
+                    GPUs
+                  </Typography>
+
+                  <Typography variant="h2" fontSize={16} sx={{ marginBottom: "5px" }}>
+                    SR-IOV: {data.gpu.vGPU ? "true" : "false"}
+                  </Typography>
+                  {data.gpu.gpus.map((val, index) => (
+                    <Typography variant="h2" fontSize={16} sx={{ marginBottom: "10px" }} key={index}>
+                      {index + 1}: {val.model}
+                    </Typography>
+                  ))}
+                </CardContent>
+              </Card>
+            </Grid2>
+          )}
+
+          {data.pcie.length > 0 && (
+            <Grid2 xs={12} sm={6} lg={3}>
+              <Card variant="outlined" sx={{ height: "270px" }}>
+                <CardContent>
+                  {/* <CardMedia sx={{ height: "160px", marginBottom: "8px" }} image={nvidia_logo} /> */}
+                  <Typography variant="h1" fontSize={22} sx={{ marginBottom: "10px" }}>
+                    PCIe Devices
+                  </Typography>
+                  {data.pcie.map((val, index) => (
+                    <React.Fragment key={index}>
+                      <Typography variant="h2" fontSize={16}>
+                        {index + 1}: {val.name}
+                      </Typography>
+                      <Typography variant="h2" fontSize={14} sx={{ textIndent: "20px", marginBottom: "10px" }}>
+                        Manufacturer: {val.manufacturer}
+                      </Typography>
+                    </React.Fragment>
+                  ))}
+                </CardContent>
+              </Card>
+            </Grid2>
+          )}
+          <Grid2 xs={12} sm={6} lg={3}>
+            <Card variant="outlined" sx={{ height: "270px" }}>
+              <CardContent>
+                <TableContainer>
+                  <Table size="small" sx={{ tableLayout: "fixed", width: `${diskChassisArr.length * 40}px` }}>
+                    <TableHead>
+                      <TableRow>
+                        {diskChassisArr.length > 0 &&
+                          diskChassisArr.map((val, index) => (
+                            <TableCell align="center" key={index}>
+                              {index}
+                            </TableCell>
+                          ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        {diskChassisArr.length > 0 &&
+                          diskChassisArr.map((val, index) => (
+                            <TableCell
+                              key={index}
+                              sx={{
+                                border: 1,
+                                borderColor: grey[300],
+                                textOrientation: "mixed",
+                                writingMode: "vertical-rl",
+                              }}
+                            >
+                              {val.status !== "empty" && (
+                                <Box sx={{ display: "flex", alignItems: "center" }}>
+                                  <i
+                                    className="bi bi-activity"
+                                    style={{
+                                      color: val.status === "OK" ? green[300] : orange[300],
+                                      fontSize: "15px",
+                                      marginBottom: "10px",
+                                    }}
+                                  />
+                                  <Typography fontSize={14}>{val.name}</Typography>
+                                </Box>
+                              )}
+                            </TableCell>
+                          ))}
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </CardContent>
             </Card>
           </Grid2>
@@ -144,7 +301,7 @@ const Redfish = ({ query }) => {
             <Divider />
           </Grid2>
           {data.network.map((val, index) => (
-            <Grid2 xs={12} sm={6} md={4} lg={3} key={index}>
+            <Grid2 xs={12} sm={6} lg={3} key={index}>
               <Card variant="outlined">
                 <CardContent>
                   <Typography variant="h1" fontSize={18}>
@@ -160,14 +317,14 @@ const Redfish = ({ query }) => {
                     MAC: {val.mac}
                   </Typography>
                   <Typography variant="h2" fontSize={16}>
-                    Speed: {val.speed}
+                    Speed: {val.speed / 1000} Gb
                   </Typography>
                 </CardContent>
               </Card>
             </Grid2>
           ))}
         </Grid2>
-        <br />
+        {/* <br />
         <TableContainer>
           <Table>
             <TableHead></TableHead>
@@ -395,7 +552,7 @@ const Redfish = ({ query }) => {
               </TableRow>
             </TableBody>
           </Table>
-        </TableContainer>
+        </TableContainer> */}
       </Box>
     </>
   )
