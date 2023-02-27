acd0fe6f54ebd080c3e3f543d794301f214c936a
jafurlan <joshdog406@gmail.com>
2023-02-27 03:17:04 -0500

    fixed adding objects to settings array

--- client/src/routes/Settings.jsx
+++ client/src/routes/Settings.jsx
@@ -1,8 +1,10 @@
-import { Button, TextField, Typography } from "@mui/material"
+import { Button, IconButton, TextField, Typography } from "@mui/material"
 import { Controller, useFieldArray, useForm } from "react-hook-form"
 import React, { useContext } from "react"
 
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined"
 import BgContainer from "../components/BgContainer"
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 import { PluginContext } from "../contexts/PluginContext"
 import { UserContext } from "../contexts/UserContext"
@@ -149,31 +151,49 @@ const Settings = () => {
       category: "Firmware Versions",
       fields: firmware_fields,
       prefix: "bmc.firmware_versions",
+      add_fields: firmware_append,
+      remove_fields: firmware_remove,
+      contains: { name: "", model: "", bios: "", bmc: "" },
     },
     {
       category: "Floorplan Tag Mapping",
       fields: tag_mapping_fields,
       prefix: "floorplan.tag_mapping",
+      add_fields: tag_mapping_append,
+      remove_fields: tag_mapping_remove,
+      contains: { tag: "", color: "" },
     },
     {
       category: "Switch Model Colors",
       fields: model_color_fields,
       prefix: "floorplan.model_color",
+      add_fields: model_color_append,
+      remove_fields: model_color_remove,
+      contains: { display: "", color: "", model: "" },
     },
     {
       category: "Switch Version Color",
       fields: version_color_fields,
       prefix: "floorplan.version_color",
+      add_fields: version_color_append,
+      remove_fields: version_color_remove,
+      contains: { display: "", color: "", version: "" },
     },
     {
       category: "Node Prefixes",
       fields: rack_prefix_fields,
       prefix: "rack.prefix",
+      add_fields: rack_prefix_append,
+      remove_fields: rack_prefix_remove,
+      contains: { type: "", prefix: "" },
     },
     {
       category: "Node Sizes",
       fields: node_size_fields,
       prefix: "rack.node_size",
+      add_fields: node_size_append,
+      remove_fields: node_size_remove,
+      contains: { models: "", height: 1, width: 1 },
     },
   ]
 
@@ -186,7 +206,7 @@ const Settings = () => {
           <Typography>Heorot version: {plugins.version}</Typography>
         </Grid2>
         <Grid2 xs={6} sx={{ display: "flex", justifyContent: "center" }}>
-          <Button variant="outlined" size="small" onClick={() => query.refetch()}>
+          <Button variant="outlined" size="small" onClick={() => query_updates.refetch()}>
             Check for Updates
           </Button>
         </Grid2>
@@ -206,17 +226,24 @@ const Settings = () => {
         <Grid2 container spacing={2}>
           {settings_form.map((item, index) => (
             <React.Fragment key={index}>
-              <Grid2 xs={item.prefix ? 12 : 6}>{item.category}</Grid2>
+              <Grid2 xs={item.prefix ? 12 : 6}>
+                {item.category}
+                {item.prefix && (
+                  <IconButton onClick={() => item.add_fields(item.contains)}>
+                    <AddOutlinedIcon />
+                  </IconButton>
+                )}
+              </Grid2>
               <Grid2 xs={item.prefix ? 12 : 6}>
                 {item.fields.map((settings_field, index) => {
                   if (item.prefix !== undefined) {
                     // settings in Array
                     return (
-                      <Grid2 container spacing={2} key={index}>
+                      <Grid2 container spacing={2} key={settings_field.id}>
                         {Object.keys(settings_field)
                           .filter((val) => val !== "id")
                           .map((val) => (
-                            <Grid2 xs={3} key={item.id + "-" + val}>
+                            <Grid2 xs={2} key={item.id + "-" + val}>
                               <Controller
                                 name={`${item.prefix}.${index}.${val}`}
                                 control={control}
@@ -224,6 +251,11 @@ const Settings = () => {
                               />
                             </Grid2>
                           ))}
+                        <Grid2 xs={2}>
+                          <IconButton onClick={() => item.remove_fields(index)}>
+                            <CloseOutlinedIcon />
+                          </IconButton>
+                        </Grid2>
                       </Grid2>
                     )
                   } else {
