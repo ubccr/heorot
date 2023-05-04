8179f5edda3732ed1b1742a3420d0db34cadb560
jafurlan <joshdog406@gmail.com>
2023-05-04 11:11:24 -0400

    added return and refresh buttons

--- src/pages/host/[host].tsx
+++ src/pages/host/[host].tsx
@@ -1,21 +1,39 @@
+import { ArrowPathIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+
 import Grendel from "~/components/node/grendel";
+import Link from "next/link";
 // import Backups from "~/components/node/backups";
 import type { NextPage } from "next";
 import PortManagement from "~/components/node/portManagement";
 import { Tab } from "@headlessui/react";
+import { api } from "~/utils/api";
 import { useRouter } from "next/router";
 
 const Node: NextPage = () => {
   const router = useRouter();
   const host = router.query.host as string;
+  const rack = host.split("-")[1] ?? "";
 
   function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
 
+  const host_res = api.grendel.host.find.useQuery(host);
+
   return (
     <>
-      <h1>{host}</h1>
+      <div className="flex items-center justify-center gap-2">
+        <Link href={`/rack/${rack}`}>
+          <ArrowUturnLeftIcon className="h-5 w-5 hover:text-neutral-200" />
+        </Link>
+        <h1 className="text-xl">{host}</h1>
+        <ArrowPathIcon
+          className={`h-5 w-5 hover:text-neutral-200 ${
+            host_res.isLoading ? "animate-spin" : ""
+          }`}
+          onClick={() => void host_res.refetch()}
+        />
+      </div>
       <br />
       <Tab.Group>
         <Tab.List className="flex justify-center gap-3 ">
