52df476866ad248af2d4160a3133051b4cfac8a9
jafurlan <joshdog406@gmail.com>
2023-05-05 15:46:45 -0400

    updated port selection UI

--- src/components/host/portManagement.tsx
+++ src/components/host/portManagement.tsx
@@ -6,16 +6,21 @@ import ProgressBar from "../progressbar";
 import { Tooltip } from "react-tooltip";
 import { api } from "~/utils/api";
 import { toast } from "react-toastify";
-import { useForm } from "react-hook-form";
+
+// import { useForm } from "react-hook-form";
 
 const PortManagement = ({ host }: { host: string }) => {
   const interfaces = api.switches.interface.list.useQuery(host);
-  const interface_mutation = api.switches.interface.refresh.useMutation();
+  const interface_mutation = api.switches.interface.refresh.useMutation({
+    onSuccess: () => {
+      toast.success("Successfully refreshed interfaces");
+    },
+  });
   const config_interface_mutation =
     api.switches.interface.configure.useMutation({
       onSuccess: () => {
         setOpen(true);
-        // refresh();
+        // await refresh();
         // toast.success("Successfully updated interfaces");
       },
       onError: (error) => {
@@ -30,45 +35,62 @@ const PortManagement = ({ host }: { host: string }) => {
     await interfaces.refetch();
   }
 
-  function addUpdatePort(port: string, checked: boolean) {
-    if (checked) setUpdatePortArr([...updatePortArr, port]);
-    if (!checked)
-      setUpdatePortArr(updatePortArr.filter((ports) => ports !== port));
+  function addUpdatePort(port: string) {
+    const found = updatePortArr.find((updatePort) => updatePort === port);
+
+    if (!found) setUpdatePortArr([...updatePortArr, port]);
+    else setUpdatePortArr(updatePortArr.filter((ports) => ports !== port));
   }
 
-  type FormData = {
-    type: string;
-    description: string;
-    uplink_speed?: number;
-    vlan_id: number;
-    port_channel?: number;
-  };
+  // type FormData = {
+  //   type: string;
+  //   description: string;
+  //   uplink_speed?: number;
+  //   vlan_id: number;
+  //   port_channel?: number;
+  // };
 
-  const { register, handleSubmit } = useForm<FormData>();
+  // const { register, handleSubmit } = useForm<FormData>();
 
-  const onSubmit = (data: FormData) => {
-    config_interface_mutation.mutate({
-      host: host,
-      type: data.type,
-      interfaces: updatePortArr,
-      description: data.description,
-      vlan_id: data.vlan_id,
-      uplink_speed: data.uplink_speed,
-      port_channel: data.port_channel,
-    });
-  };
+  // const onSubmit = (data: FormData) => {
+  //   config_interface_mutation.mutate({
+  //     host: host,
+  //     type: data.type,
+  //     interfaces: updatePortArr,
+  //     description: data.description,
+  //     vlan_id: data.vlan_id,
+  //     uplink_speed: data.uplink_speed,
+  //     port_channel: data.port_channel,
+  //   });
+  // };
 
   return (
     <div>
       <div className="flex flex-col justify-center gap-3">
-        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises*/}
-        <button type="button" onClick={() => refresh()}>
-          Refresh
-        </button>
+        <div className="flex justify-center gap-3">
+          <button
+            type="button"
+            /* eslint-disable-next-line @typescript-eslint/no-misused-promises*/
+            onClick={() => refresh()}
+            className="rounded-md border px-2 hover:bg-neutral-700 dark:border-white"
+          >
+            Refresh
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setUpdatePortArr([]);
+              toast.success("Successfully cleared port selection");
+            }}
+            className="rounded-md border px-2 hover:bg-neutral-700 dark:border-white"
+          >
+            Clear Selection
+          </button>
+        </div>
         {interfaces.isLoading ||
           (interface_mutation.isLoading && <ProgressBar />)}
       </div>
-      <table className="table table-fixed border-separate">
+      <table className="table-fixed border-separate">
         <tbody>
           {interfaces.data?.map((blade, index) => (
             <React.Fragment key={`blade-${index}`}>
@@ -77,22 +99,24 @@ const PortManagement = ({ host }: { host: string }) => {
               </tr>
               <tr>
                 {blade.map((port, index) => (
-                  <PortGen
+                  <BladeGen
                     port={port}
                     index={index}
                     display="even"
                     addUpdatePort={addUpdatePort}
+                    updatePortArr={updatePortArr}
                     key={`port-${index}`}
                   />
                 ))}
               </tr>
               <tr>
                 {blade.map((port, index) => (
-                  <PortGen
+                  <BladeGen
                     port={port}
                     index={index}
                     display="odd"
                     addUpdatePort={addUpdatePort}
+                    updatePortArr={updatePortArr}
                     key={`port-${index}`}
                   />
                 ))}
@@ -108,8 +132,7 @@ const PortManagement = ({ host }: { host: string }) => {
             config_interface_mutation.data}
         </pre>
       </Modal>
-      <form
-        /* eslint-disable-next-line @typescript-eslint/no-misused-promises*/
+      {/* <form
         onSubmit={handleSubmit(onSubmit)}
         className="grid grid-cols-2 gap-3 max-md:grid-cols-1"
       >
@@ -167,22 +190,24 @@ const PortManagement = ({ host }: { host: string }) => {
         </div>
 
         <button type="submit">Submit Port(s)</button>
-      </form>
+      </form> */}
     </div>
   );
 };
 export default PortManagement;
 
-export const PortGen = ({
+const BladeGen = ({
   port,
   index,
   display,
   addUpdatePort,
+  updatePortArr,
 }: {
   port: InterfaceStatus[];
   index: number;
   display: string;
-  addUpdatePort: (port: string, checked: boolean) => void;
+  addUpdatePort: (port: string) => void;
+  updatePortArr: string[];
 }) => {
   const iface = port[0];
   if (!iface) return <td></td>;
@@ -191,14 +216,22 @@ export const PortGen = ({
     <React.Fragment key={`port-${index}`}>
       {index === 0 && display === "even" && (
         <td rowSpan={2} className="border border-white">
-          {iface.port.replace(/Ethernet/gi, "").split("/")[0]}
+          {iface.port?.split("/")[0]}
         </td>
       )}
       {display === "even" && index % 2 === 0 && (
-        <Ports iface={iface} addUpdatePort={addUpdatePort} />
+        <Ports
+          iface={iface}
+          addUpdatePort={addUpdatePort}
+          updatePortArr={updatePortArr}
+        />
       )}
       {display === "odd" && index % 2 === 1 && (
-        <Ports iface={iface} addUpdatePort={addUpdatePort} />
+        <Ports
+          iface={iface}
+          addUpdatePort={addUpdatePort}
+          updatePortArr={updatePortArr}
+        />
       )}
     </React.Fragment>
   );
@@ -207,9 +240,11 @@ export const PortGen = ({
 const Ports = ({
   iface,
   addUpdatePort,
+  updatePortArr,
 }: {
   iface: InterfaceStatus;
-  addUpdatePort: (port: string, checked: boolean) => void;
+  addUpdatePort: (port: string) => void;
+  updatePortArr: string[];
 }) => {
   let border_color = "border-white";
   if (iface.line_protocol_status === "down") border_color = "border-red-600";
@@ -230,7 +265,7 @@ const Ports = ({
   )
     border_color = "border-teal-400";
   const tooltip_table = [
-    { title: "Port", value: iface.port },
+    { title: "Port", value: iface.port_name },
     { title: "Description", value: iface.description },
     { title: "Line Protocol", value: iface.line_protocol_status },
     { title: "Link Status", value: iface.link_status },
@@ -242,7 +277,14 @@ const Ports = ({
   return (
     <td
       data-tooltip-id={iface.id}
-      className={`h-8 w-36 border ${border_color} hover:border-white hover:bg-neutral-500`}
+      width={40}
+      height={20}
+      className={`border ${border_color} rounded-sm hover:bg-neutral-700 ${
+        updatePortArr.find((port) => port === iface.port_name)
+          ? "border-blue-500 bg-neutral-500"
+          : ""
+      }`}
+      onClick={() => addUpdatePort(iface.port_name)}
     >
       <Tooltip id={iface.id}>
         <table className="table-auto">
@@ -259,14 +301,7 @@ const Ports = ({
           </tbody>
         </table>
       </Tooltip>
-      {iface.port.split("/")[1]}
-      <input
-        type="checkbox"
-        className="float-right"
-        onChange={(event) =>
-          addUpdatePort(iface?.port ?? "", event.target.checked)
-        }
-      />
+      {iface.port?.split("/")[1]}
     </td>
   );
 };
