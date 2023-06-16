711344ed6fe2bd5285e37f62175c0974246fc407
jafurlan <joshdog406@gmail.com>
2023-06-16 12:52:12 -0400

    updated config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  tabWidth: 2,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 120
 };
 
 module.exports = config;
